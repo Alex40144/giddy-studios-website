7af2abf6ff4c56890262cbcb1b40ca7b1c79c913
GiddyRaccoon <giddyraccoon@gmail.com>
2020-04-25 12:41:44 +0100

    more refactoring

--- Throttlereport.php
+++ Throttlereport.php
@@ -1,8 +1,8 @@
 <?php
-    if(isset($_POST["passengers"])){
-    echo "Number of selected passengers are:".$_POST["passengers"];
-    $ret = file_put_contents('data.txt', $_POST["passengers"], LOCK_EX);
+    if(isset($_POST["Throttle"])){
+    echo "Throttle Percentage is:".$_POST["Throttle"];
+    $ret = file_put_contents('data.txt', $_POST["Throttle"], LOCK_EX);
     } else{
-    Echo "Please slide the Slider Bar and Press Submit.";
+    Echo "Please slide the Slider Bar.";
     }
     ?>
